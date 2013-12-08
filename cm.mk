9223d4dbb1c68f961d3da06afd590fe96f431592
Austin Widner <austinw125@gmail.com>
2013-12-07 22:03:56 -0600

    update for a100

--- cm.mk
+++ cm.mk
@@ -1,25 +1,22 @@
 # Release name
-PRODUCT_RELEASE_NAME := A500
+PRODUCT_RELEASE_NAME := A100
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/acer/a500/a500.mk)
+$(call inherit-product, device/acer/a100/a100.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE       := a500
-PRODUCT_NAME         := cm_a500
+PRODUCT_DEVICE       := a100
+PRODUCT_NAME         := cm_a100
 PRODUCT_BRAND        := Acer
-PRODUCT_MODEL        := A500
+PRODUCT_MODEL        := A100
 PRODUCT_MANUFACTURER := Acer
 
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=picasso \
-    BUILD_FINGERPRINT="acer/a500_ww_gen1/picasso:4.0.3/IML74K/1329824557:user/release-keys" \
-    PRIVATE_BUILD_DESC="a500_ww_gen1-user 4.0.3 IML74K 1329824557 release-keys"
-
-# Bootanimation
-PRODUCT_BOOTANIMATION := vendor/cm/prebuilt/common/bootanimation/800.zip
+    PRODUCT_NAME=vangogh \
+    BUILD_FINGERPRINT="acer/a100_ww_gen1/vangogh:4.0.3/IML74K/1332408262:user/release-keys" \
+    PRIVATE_BUILD_DESC="a100_ww_gen1-user 4.0.3 IML74K 1332408262 release-keys"
 
